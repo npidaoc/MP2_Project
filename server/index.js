fb3d768ad46917f7f8f809daeae5eed3749a94b5
npidaoc <153940252+npidaoc@users.noreply.github.com>
2024-03-10 23:31:21 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 10000;
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
