fb4f8b6448606ac706a8e14b5e152d4438ca4ee0
npidaoc <npidaoc@gmail.com>
2024-03-10 23:26:50 +0800

    modified

--- server/index.js
+++ server/index.js
@@ -66,6 +66,6 @@ app.get('/api/data', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
