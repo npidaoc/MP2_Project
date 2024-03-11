0b25b5bc2c831a55a4c1fedc451ee77ffc299510
npidaoc <npidaoc@gmail.com>
2024-03-11 13:29:08 +0800

    change committed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
-// import About from "./Components/About";
 import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
